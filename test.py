9305ab0e07bda06fa218d6e4a73880cc22be368a
cristianoBY <boyu2844240635@gmail.com>
2019-03-25 18:24:02 -0400

    clean up

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def main():
 	'''
 	parse all the data
 	wsd_data[idx]: ordered dic {['split', 'train'], ['Annotator.ID', '0'], ...['Display.Position', '6']}
-	train_data[sentence_idx]: the tokenized sentence at idx from EUD
+	train_data[sentence_idx]: a list of tokenized sentence at idx from EUD
 	train_data[sentence_idx][word_idx]: an ordered dict for the word at this index
 	in the form: OrderedDict([('id', 14), ('form', 'the'), ('lemma', 'the'), ...])
 	same for dev and test
@@ -27,33 +27,38 @@ def main():
 	elmo = Elmo(options_file, weight_file, 1, dropout=0)
 
 	# test on the first 10 words
-	for i in range(10):
+	all_sentences = []
+	for i in range(1):
 
+		# get the original sentence from EUD
 		sentence_id = wsd_data[i].get('Sentence.ID')
 
 		# the index in EUD is 1-based!!!
 		sentence_number = int(sentence_id.split()[-1]) - 1
 		word_index = int(wsd_data[i].get('Arg.Token')) - 1
 		word_lemma = wsd_data[i].get('Arg.Lemma')
+		sentence = []
 
 		if "train" in sentence_id:
 
-			sentence = list(train_data[sentence_number])
+			sentence = train_data[sentence_number]
 			# print(sentence)
 			assert(sentence[word_index].get('lemma') == word_lemma)
 
 		elif "test" in sentence_id:
 
-			sentence = list(test_data[sentence_number])
+			sentence = test_data[sentence_number]
 			# print(sentence)
 			assert(sentence[word_index].get('lemma') == word_lemma)
-
 
 		else:
 
-			sentence = list(dev_data[sentence_number])
+			sentence = dev_data[sentence_number]
 			# print(sentence)
 			assert(sentence[word_index].get('lemma') == word_lemma)
+
+		all_sentences.append(sentence)
+		print(all_sentences)
 
 if __name__ == '__main__':
 	main()
